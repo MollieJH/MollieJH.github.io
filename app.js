8ab5e22ee2b0b32b7a458c4025bc77c7d1315c67
MollieJH <mollie_hornby@outlook.com>
2024-09-12 01:23:47 +0100

    mojo bakes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const images = document.querySelectorAll("img");
+    const images = document.querySelector("footer.img.doggos");
 
     for(const image of images){
         fetch("https://dog.ceo/api/breeds/image/random")
